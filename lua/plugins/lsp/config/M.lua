8338187dd40e4112d2f4aca11a8ecb9038af177b
fzb <1466518303@qq.com>
2023-04-24 04:25:05 +0800

    traverse lspserver

--- lua/plugins/lsp/config/M.lua
+++ lua/plugins/lsp/config/M.lua
@@ -1,17 +1,29 @@
 local M = {}
-function r(client)
+local function r(client)
 	return require("plugins.lsp.config." .. client)
 end
-
-M.html = r("html")
-M.cssls = r("css")
-M.cssmodules_ls = r("cssmodules_ls")
-M.jsonls = r("json")
-M.lua_ls = r("lua")
-M.emmet_ls = r("emmet_ls")
-M.ts = r("typescript")
-M.gopls = r("go")
-M.volar = r("vue")
-M.tailwindcss = r("tailwindcss")
+local myServer = require("plugins.lsp.config.myServer")
+for server, conf in pairs(myServer) do
+	local enable = conf.enable or true
+	if enable then
+		local status, config = pcall(r, server)
+		if status then
+			M[server] = config
+		else
+			M[server] = {}
+		end
+	end
+end
+-- M.html = r("html")
+-- M.cssls = r("cssls")
+-- M.cssmodules_ls = r("cssmodules_ls")
+-- M.jsonls = r("jsonls")
+-- M.lua_ls = r("lua")
+-- M.emmet_ls = r("emmet_ls")
+-- M.tsserver = r("tsserver")
+-- M.gopls = r("gopls")
+-- M.volar = r("volar")
+-- M.tailwindcss = r("tailwindcss")
+-- M.marksman = r("marksman")
 -- M.eslint = r('eslint')
 return M
