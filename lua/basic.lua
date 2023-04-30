e21564179b156f3d432e1a3adb4e686ae52d8ab9
fzb <1466518303@qq.com>
2023-05-01 04:25:15 +0800

    upd basic settings

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,94 +1,95 @@
--- utf8
-vim.g.encoding = "UTF-8"
-vim.o.fileencoding = "utf-8"
--- jkhl 移动时光标周围保留8行
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
--- set termguicolors
--- 使用相对行号
-vim.wo.number = true
-vim.wo.relativenumber = true
--- 高亮所在行
-vim.wo.cursorline = true
--- 显示左侧图标指示列
---vim.wo.signcolumn = "no"
-vim.bo.textwidth = 120
--- 右侧参考线，超过表示代码太长了，考虑换行
--- vim.wo.colorcolumn = "120"
--- 缩进2个空格等于一个Tab
-vim.o.tabstop = 2
-vim.bo.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftround = true
-vim.o.syntax = "enable"
--- >> << 时移动长度
-vim.o.shiftwidth = 2
-vim.bo.shiftwidth = 2
--- 空格替代tab
-vim.o.expandtab = true
-vim.bo.expandtab = true
--- 新行对齐当前行
-vim.o.autoindent = true
-vim.bo.autoindent = true
-vim.o.smartindent = true
--- 搜索大小写不敏感，除非包含大写
-vim.o.ignorecase = true
-vim.o.smartcase = true
--- 搜索不要高亮
-vim.o.hlsearch = false
--- 边输入边搜索
-vim.o.incsearch = true
--- 命令行高为2，提供足够的显示空间
-vim.o.cmdheight = 2
--- 当文件被外部程序修改时，自动加载
-vim.o.autoread = true
-vim.bo.autoread = true
--- 禁止折行
-vim.wo.wrap = true
--- 光标在行首尾时<Left><Right>可以跳到下一行
-vim.o.whichwrap = "<,>,[,]"
--- 允许隐藏被修改过的buffer
--- vim.o.hidden = true
--- 鼠标支持
-vim.opt.mouse = "a"
--- 禁止创建备份文件
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.swapfile = false
--- smaller updatetime
-vim.o.updatetime = 100
--- 设置 timeoutlen 为等待键盘快捷键连击时间500毫秒，可根据需要设置
-vim.o.timeoutlen = 350
--- split window 从下边和右边出现
-vim.o.splitbelow = true
-vim.o.splitright = true
--- 自动补全不自动选中
-vim.g.completeopt = "menu,menuone,noselect,noinsert"
+local opt = vim.opt
+local g = vim.g
+g.encoding = "UTF-8"
+opt.fileencoding = "utf-8"
+opt.autowrite = true -- 启用自动写入
 -- 样式
-vim.o.background = "dark"
-vim.o.termguicolors = true
-vim.opt.termguicolors = true
--- 不可见字符的显示，这里只把空格显示为一个点
-vim.o.nolist = false
-vim.o.list = false
--- vim.o.listchars = "tab:··,space:·"
-
+opt.background = "dark" --dark light
+-- 真色支持
+opt.termguicolors = true
+-- 禁止折行
+opt.wrap = true
+-- 光标在行首尾时<Left><Right>可以跳到下一行
+opt.whichwrap = "<,>,[,]"
 -- 补全增强
-vim.o.wildmenu = true
--- Dont' pass messages to |ins-completin menu|
-vim.o.shortmess = vim.o.shortmess .. "c"
+opt.wildmenu = true
+-- 自动补全不自动选中
+-- g.completeopt = "menu,menuone,noselect,noinsert"
+opt.completeopt = "menu,menuone,noselect"
 -- 补全最多显示10行
-vim.o.pumheight = 10
+opt.pumblend = 10 -- 弹出混合
+opt.pumheight = 10 -- 弹出窗口中的最大条目数
 -- 永远显示 tabline
-vim.o.showtabline = 2
+opt.showtabline = 2
 -- 使用增强状态栏插件后不再需要 vim 的模式提示
-vim.o.showmode = false
-vim.o.ttimeoutlen = 100
-vim.g.loaded_perl_provider = 0
+opt.showmode = false
+opt.ttimeoutlen = 100
+g.loaded_perl_provider = 0
+opt.splitbelow = true -- 将新窗口置于当前窗口之下
+opt.splitright = true -- 将新窗口放在当前窗口的右边
+-- jkhl 移动时光标周围保留8行
+opt.scrolloff = 4
+opt.sidescrolloff = 8
+-- 使用相对行号
+opt.number = true
+opt.relativenumber = true
+-- 高亮所在行
+opt.cursorline = true
+-- 显示左侧图标指示列
+opt.signcolumn = "yes"
+opt.textwidth = 120
+-- 空格替代tab
+opt.expandtab = true
+-- 缩进2个空格等于一个Tab
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftround = true
+-- 缩进的大小
+opt.shiftwidth = 2
+opt.smartindent = true -- 自动插入缩进
+opt.syntax = "enable" --开启代码高亮
+-- 按键 前缀 空格
+g.mapleader = " "
+g.maplocalleader = " "
+-- 在退出修改缓冲区之前确认保存更改
+opt.confirm = true
+-- 鼠标支持
+opt.mouse = "a"
 --鼠标事件
-vim.opt.mousemoveevent = true
--- 前缀 空格
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+opt.mousemoveevent = true
+-- smaller updatetime
+opt.updatetime = 100
+-- 设置 timeoutlen 为等待键盘快捷键连击时间300毫秒，可根据需要设置
+opt.timeoutlen = 300
+-- 搜索大小写不敏感，除非包含大写
+opt.ignorecase = true
+opt.smartcase = true
+-- 不可见字符的显示
+-- vim.opt.nolist = false
+opt.list = false
+opt.shortmess:append({ W = true, I = true, c = true })
+-- Dont' pass messages to |ins-completin menu|
+-- vim.opt.shortmess = vim.opt.shortmess .. "c"
+-- 新行对齐当前行
+opt.autoindent = true
+opt.smartindent = true
+-- 边输入边搜索
+opt.incsearch = true
+-- 当文件被外部程序修改时，自动加载
+opt.autoread = true
+-- 禁止创建备份文件
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
 --剪贴板
-vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
+-- vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
+opt.clipboard = "unnamedplus"
+opt.splitkeep = "screen" --决定打开、关闭或调整横向分割的尺寸时的滚动行 文本保持在相同屏幕行。
+--可撤销的最大次数
+opt.undolevels = 10000
+--自动保存撤销历史
+opt.undofile = true
+--命令行的补全模式
+opt.wildmode = "longest:full,full"
+-- Fix markdown indentation settings
+g.markdown_recommended_style = 0
