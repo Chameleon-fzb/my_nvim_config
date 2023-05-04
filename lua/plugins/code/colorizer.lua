410f459e96aecbede80db58ba41dd96cfcd0fa09
fzb <1466518303@qq.com>
2023-05-04 14:42:49 +0800

    use ccc 1ce

--- lua/plugins/code/colorizer.lua
+++ lua/plugins/code/colorizer.lua
@@ -1,6 +1,7 @@
 -- color高亮显示
 return {
 	"NvChad/nvim-colorizer.lua",
+	enabled = false,
 	event = { "CursorHold" },
 	opts = {
 		options = {
@@ -15,7 +16,6 @@ return {
 				hsl_fn = true, -- CSS hsl() and hsla() functions
 				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
 				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-				-- Available modes for `mode`: foreground, background,  virtualtext
 				mode = "background", -- Set the display mode.
 				-- Available methods are false / true / "normal" / "lsp" / "both"
 				-- True is same as normal
