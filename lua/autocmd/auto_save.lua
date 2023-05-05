d24b721127c0ea811c2a50df554f59ac29ef4f04
fzb <1466518303@qq.com>
2023-05-06 02:46:30 +0800

    fix bug

--- lua/autocmd/auto_save.lua
+++ lua/autocmd/auto_save.lua
@@ -1,10 +1,17 @@
 return function(conf)
 	local delay = conf.save_delay or 1000
 	local events = conf.events or { "InsertLeave", "TextChanged" }
+	local timer = false
+	local clear_timer = function() end
+	if timer then
+		vim.loop.timer_stop(timer)
+		timer = false
+	end
 	vim.api.nvim_create_autocmd(events, {
 		pattern = { "*" },
 		callback = function()
-			local timer = vim.loop.new_timer()
+			clear_timer()
+			timer = vim.loop.new_timer()
 			timer:start(
 				delay,
 				0,
@@ -12,13 +19,11 @@ return function(conf)
 					vim.cmd("silent! wall")
 				end)
 			)
-			vim.api.nvim_create_autocmd("InsertEnter", {
-				pattern = { "*" },
-				callback = function()
-					vim.loop.timer_stop(timer)
-				end,
-			})
 		end,
 		nested = true,
+	})
+	vim.api.nvim_create_autocmd("InsertEnter", {
+		pattern = { "*" },
+		callback = clear_timer,
 	})
 end
